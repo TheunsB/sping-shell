b1dd0a0147878f85246647de73036dc30edaebc0
Theunsb <theunsb32@gmail.com>
2021-11-01 18:08:32 +0100

    removed unused test

--- src/test/java/breytenbach/theuns/example/springshell/SpringshellApplicationTests.java
+++ src/test/java/breytenbach/theuns/example/springshell/SpringshellApplicationTests.java
@@ -1,13 +1,8 @@
 package breytenbach.theuns.example.springshell;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringshellApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
